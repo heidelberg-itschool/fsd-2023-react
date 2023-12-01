98d2a311b2bec70551410261fb4361a8c99ae098
Andrea Angelova <andrea.angelova@swisscom.com>
2023-12-01 19:08:53 +0100

    add simple router

--- day05/src/App.js
+++ day05/src/App.js
@@ -1,11 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Form from './components/exercises/Form';
+import Home from './pages/Home';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Form></Form>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/dashboard' element={<Dashboard/>} />
+          <Route path='/form' element={<Form/>} />
+          <Route path='*' element={<NotFound/>} />
+        </Routes>
       </header>
     </div>
   );
