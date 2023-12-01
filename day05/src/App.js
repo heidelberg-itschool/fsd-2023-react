469f228c01ec917c4d058b804225cc124c627c44
Andrea Angelova <andrea.angelova@swisscom.com>
2023-12-01 19:36:54 +0100

    add layout router

--- day05/src/App.js
+++ day05/src/App.js
@@ -5,17 +5,20 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Dashboard from './pages/Dashboard';
 import NotFound from './pages/NotFound';
+import Layout from './layouts/Layout';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/about' element={<About/>} />
-          <Route path='/dashboard' element={<Dashboard/>} />
-          <Route path='/form' element={<Form/>} />
-          <Route path='*' element={<NotFound/>} />
+          <Route path='/' element={<Layout/>}>
+            <Route index element={<Home/>} />
+            <Route path='/about' element={<About/>} />
+            <Route path='/dashboard' element={<Dashboard/>} />
+            <Route path='/form' element={<Form/>} />
+            <Route path='*' element={<NotFound/>} />
+          </Route>
         </Routes>
       </header>
     </div>
