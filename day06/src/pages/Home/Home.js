778fbea90e43117b6a827d7a90d25591aa0d1b71
Andrea Angelova <andrea.angelova@swisscom.com>
2023-12-04 19:37:18 +0100

    add images

--- day06/src/pages/Home/Home.js
+++ day06/src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import './Home.scss';
 import groupClasses from '../../utils/groupClasses';
+import welcomeImg from './../../assets/images/welcome.jpeg';
 
 function Home({active}) {
     const paragraphStyle = ['paragraph', 'large', active && 'active-paragraph'].join(" ");
@@ -12,6 +13,11 @@ function Home({active}) {
         <p className={`paragraph ${active ? 'active-paragraph' : ''}`}>This is styled paragraph</p>
         <p className={paragraphStyle}>This is styled paragraph</p>
         <p className={groupClasses('paragraph', 'large', active && 'active-paragraph')}>This is styled paragraph</p>
+
+        <img style={{width: '300px'}}
+         src='https://t3.ftcdn.net/jpg/05/23/04/00/360_F_523040057_JYMTxoQGquklUthNLLjspI7ldR1hrFlH.jpg'
+          alt='welcome' />
+        <img style={{width: '300px'}} src={welcomeImg} alt='welcome2' />
 
         <Link to='/about'>Go to About</Link>
     </div> );
