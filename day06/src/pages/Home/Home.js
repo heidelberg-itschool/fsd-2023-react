7e1d83d14379b16253e8ee101db1e32daa95cbf9
Andrea Angelova <andrea.angelova@swisscom.com>
2023-12-04 18:44:17 +0100

    add sass

--- day06/src/pages/Home/Home.js
+++ day06/src/pages/Home/Home.js
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
-import './Home.css';
+import './Home.scss';
 
 function Home() {
-    return ( <>
+    return ( <div id="home">
         <h1 id='home-title'>Home</h1>
         <p>This is home page</p>
         <Link to='/about'>Go to About</Link>
-    </> );
+    </div> );
 }
 
 export default Home;
