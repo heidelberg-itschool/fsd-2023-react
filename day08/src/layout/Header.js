6478d862b256d0fac0601e0bb7d078d71718906d
Andrea Angelova <andrea.angelova@swisscom.com>
2023-12-14 20:17:12 +0100

    add logout function

--- day08/src/layout/Header.js
+++ day08/src/layout/Header.js
@@ -1,13 +1,23 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import UserContext from "../context/UserContext";
 
 function Header() {
+    const { user, setUser } = useContext(UserContext);
+
+    function resetUser() {
+        setUser({});
+    }
+
     return ( 
     <div className="App-header">
         <div className="navigation">
             <Link to='/'>Home</Link>
             <Link to='/products'>Products</Link>
             <Link to='/product'>Product</Link>
-            <Link to='/login'>Log In</Link>
+            {!user.name ?
+                    <Link to='/login'>Log In</Link> : 
+                    <button onClick={resetUser}>Log out</button>}
         </div>
     </div>);
 }
