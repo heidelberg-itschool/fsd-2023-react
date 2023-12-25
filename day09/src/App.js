d9c66e623a7901b6c3c12782e13cfac0717fc081
Andrea Angelova <andrea.angelova@swisscom.com>
2023-12-25 18:10:34 +0100

    add basic rounting

--- day09/src/App.js
+++ day09/src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Product from './pages/Product';
+import Card from './pages/Card';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/products' element={<Products/>} />
+          <Route path='/product' element={<Product/>} />
+          <Route path='/card' element={<Card/>} />
+          <Route path='*' element={<NotFound/>} />
+        </Routes>
       </header>
     </div>
   );
---
+++ day09/src/pages/Card.js
@@ -0,0 +1,5 @@
+function Card() {
+    return ( <h1>Card page</h1> );
+}
+
+export default Card;
